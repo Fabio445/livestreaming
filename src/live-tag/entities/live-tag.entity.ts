efc9a90004e62f5d147426ae016e14473d16f274
baddy02 <valentina.badiali2002@gmail.com>
2023-11-29 11:10:05 +0100

    8

--- src/live-tag/entities/live-tag.entity.ts
+++ src/live-tag/entities/live-tag.entity.ts
@@ -1,1 +1,13 @@
-export class LiveTag {}
+import { PrimaryGeneratedColumn, Column, Entity, ManyToOne, OneToMany } from "typeorm";
+import { Live } from "../../live/entities/live.entity";
+
+@Entity()
+export class LiveTag {
+
+	@PrimaryGeneratedColumn()
+	idLiveTag: number
+
+    @OneToMany(() => Live, live => live.categoria)
+    live: Live;
+ 
+}
